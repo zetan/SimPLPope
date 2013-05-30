f3c024c2f0e7c89a003334bcada14bbafaf55ae8
zetan <sophie_tan525@yahoo.com.cn>
2013-05-30 22:30:53 +0800

    Eval interface

--- src/syntax/Expression.java
+++ src/syntax/Expression.java
@@ -6,11 +6,23 @@ import semantics.Type;
 public class Expression
 {
 	protected Type type;
+	protected Value value;
 	public Expression() {
 		// TODO Auto-generated constructor stub
 	}
 	
 	public boolean CheckType(){return true;}
+	//implement this method and fill the real value into value
+	public void Eval(){}
+	
+	public Value getValue() {
+		return value;
+	}
+
+	public void setValue(Value value) {
+		this.value = value;
+	}
+
 	public Type getType(){
 		if(this.getClass().getName().contains("Variable")) return EnvStack.getInstance().getVarType((Variable)this);
 		else return type;
--- src/syntax/Value.java
+++ src/syntax/Value.java
@@ -1,6 +1,70 @@
 package syntax;
 
 public class Value extends Expression{
+	int intValue;
+	boolean boolValue;
+	AnonymousFunction funcValue;
+	List listValue;
+	Pair pair;
 	
+	public Value(int intValue) {
+		this.intValue = intValue;
+	}
+	
+	
+	public Value(boolean boolValue) {
+		this.boolValue = boolValue;
+	}
+
+
+	public Value(AnonymousFunction funcValue) {
+		this.funcValue = funcValue;
+	}
+
+
+	public Value(List listValue) {
+		this.listValue = listValue;
+	}
+
+
+	public Value(Pair pair) {
+		this.pair = pair;
+	}
+
+
+	public int getIntValue() {
+		return intValue;
+	}
+	public void setIntValue(int intValue) {
+		this.intValue = intValue;
+	}
+	public boolean isBoolValue() {
+		return boolValue;
+	}
+	public void setBoolValue(boolean boolValue) {
+		this.boolValue = boolValue;
+	}
+	public AnonymousFunction getFuncValue() {
+		return funcValue;
+	}
+	public void setFuncValue(AnonymousFunction funcValue) {
+		this.funcValue = funcValue;
+	}
+	public List getListValue() {
+		return listValue;
+	}
+	public void setListValue(List listValue) {
+		this.listValue = listValue;
+	}
+
+
+	public Pair getPair() {
+		return pair;
+	}
+
+
+	public void setPair(Pair pair) {
+		this.pair = pair;
+	}
 	
 }
