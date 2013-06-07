e3c06b775e494a3774efd44a9d03b2d9073c5950
qx16887723 <qx16887723@126.com>
2013-06-07 10:40:30 +0800

    "add the clone function"

--- src/semantics/Env.java
+++ src/semantics/Env.java
@@ -2,13 +2,15 @@ package semantics;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import syntax.Value;
 import syntax.Variable;
 
-public class Env {
+public class Env implements Cloneable{
 	private Set<String> varSet = new HashSet<String>();
 	private Map<String, Type> typeMap = new HashMap<String, Type>();
 	private Map<String, Value> valueMap = new HashMap<String, Value>();
@@ -54,6 +56,52 @@ public class Env {
 		this.valueMap = valueMap;
 	}
 	
-	
+	public Object clone()
+	{
+		Env cloned = null;
+		
+		try {
+			
+			
+			cloned  = (Env) super.clone();
+			cloned.typeMap = new HashMap<String, Type>();
+			Iterator<Entry<String,Type >> it1 = typeMap.entrySet().iterator();
+			while(it1.hasNext())
+			{
+				Map.Entry<String, Type> entry = it1.next();
+				String K = entry.getKey();
+				Type V = entry.getValue();
+				cloned.typeMap.put(K, V);
+			}
+			
+			cloned.valueMap = new HashMap<String, Value>();
+			Iterator<Entry<String,Value >> it2 = valueMap.entrySet().iterator();
+			while(it2.hasNext())
+			{
+				Map.Entry<String, Value> entry = it2.next();
+				String K = entry.getKey();
+				Value V = entry.getValue();
+				cloned.valueMap.put(K, V);
+			}
+			
+			
+			cloned.varSet = new  HashSet<String>();
+			Iterator it3 = varSet.iterator();
+			while(it3.hasNext())
+			{
+				
+				String K = (String) it3.next();
+				
+				cloned.varSet.add(K);
+			}
+			
+			
+			
+		} catch (CloneNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return cloned;
+	}
 	
 }
