daef50d02a6d11c54330d7ee8cfa6572e9a43821
zetan <sophie_tan525@yahoo.com.cn>
2013-06-08 18:03:16 +0800

    console & from file mode for main

--- src/SimplPope.java
+++ src/SimplPope.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 
 import syntax.Expression;
 import syntax.Value;
@@ -8,24 +10,54 @@ import lex.Lexer;
 
 public class SimplPope {
 	public static void main(String[] args){
+		if (args.length == 1 && args[0].equals("-s")) {
+			Console();
+		} else if (args.length == 2 && args[0].equals("-f")){
+			FromFile(args[1]);
+		} 
+		else{
+			System.out.println("usage: -s | -f [filename]");
+		}
+	}
+	
+	static void Console(){
 		Parser parser = null;
 		Expression expression = null;
-		try {
-			parser = new Parser(
-                new Lexer(System.in));
-            expression = (Expression) parser.parse().value;
-            //System.out.println(expression.toString());
-           
-        }
-        catch (Exception e) {
-        	String errString = "";
-        	Object info = new Object();
-        	parser.report_error(errString, info);
-            System.out.println("err string = " + errString);
-            System.out.println("info " + info);
-	    
-        }
-		 Value value = expression.Eval(null);
-         System.out.println(value.toString());
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		parser = new Parser(new Lexer(reader));
+		while(true){
+			System.out.print("SimPL>");
+			try {
+				 expression = (Expression) parser.parse().value;
+			}catch (Exception e) {
+				System.out.println("syntax error");
+				continue;
+			}
+			 Value value = expression.Eval(null);
+	         if(value != null)System.out.println(value.toString());         
+		}
 	}
+	
+	static void FromFile(String filename){
+		Parser parser = null;
+		Expression expression = null;
+		FileReader reader;
+		try {
+			reader = new FileReader(filename);
+			
+		}catch (Exception e) {
+			System.out.println("file not found");
+			return;
+		}
+		parser = new Parser(new Lexer(reader));
+		try {
+			 expression = (Expression) parser.parse().value;
+		}catch (Exception e) {
+			System.out.println("syntax error");
+			return;
+		}
+		 Value value = expression.Eval(null);
+        if(value != null)System.out.println(value.toString());    
+	}
+	
 }
